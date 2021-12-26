73dee7c03d6dd0a1b5147c8640eb86b701104bf6
Srinivas Varanasi <mr7vsik@gmail.com>
2021-12-26 21:00:46 +0530

    feat: update install script

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 echo "Initialising f1-cli installer"
 
+#Check if directory exists, if it does remove it
+if [ -d $HOME/.f1 ]
+then
+  rm -r $HOME/.f1
+fi
+
 #Creating a temporary directory
 TMP_DIR=$(mktemp -d)
 CURR_DIR=$(pwd)
@@ -24,7 +30,7 @@ then
   mkdir $TMP_DIR/f1_source && tar -xf $TMP_DIR/tempfile -C $TMP_DIR/f1_source --strip-components 1
 
   #Generating binary file
-  cd $TMP_DIR/f1_source && go build -o f1
+  cd $TMP_DIR/f1_source && go build -o ./build/f1
 
   #Moving binary file into .f1
   mkdir $HOME/.f1/bin && mv $TMP_DIR/f1_source/build/f1 $HOME/.f1/bin/
